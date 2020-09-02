a0dc2bb3f04dc59207472120b7dac160b3c3c08c
Bruno Sartori <brunosartori.webmaster@gmail.com>
2020-09-02 20:01:02 -0300

    add storybook

---
+++ .storybook/stories/components/Button.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import Button from '../../../src/components/Button'
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+  }
+} as Meta
+
+interface ButtonProps {
+  title: string
+}
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  title: 'lalalal'
+}
